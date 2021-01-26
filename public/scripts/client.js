1fe7f52550bd6b62b6ed6559e3e03c172b997f44
Sebastien <perigon@hotmail.com>
2021-01-26 22:32:58 +0000

    finishing touches

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,14 +1,29 @@
 $(document).ready(function() {
-
   const escape =  function(str) {
     let div = document.createElement('div');
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
-  }
+  };
 
-const renderTweets = function(tweets) {
-  for (let information of tweets) {
-    const $section = 
+  const sinceTime = (unixtime) => {
+    const curentTime = new Date().getTime();
+    let since = curentTime - unixtime;
+    since = Math.floor(since / 1000);
+    let minutes = Math.floor(since / 60);
+    let hours = 0;
+    if (minutes >= 60) {
+      while (minutes >= 60) {
+        minutes = minutes - 60;
+        hours ++;
+      }
+    }
+    return "Posted " + hours + " h " + minutes + " m ago";
+  };
+
+
+  const renderTweets = function(tweets) {
+    for (let information of tweets) {
+      const $section =
     `
     <article>
       <div class="article-head">
@@ -19,7 +34,7 @@ const renderTweets = function(tweets) {
       <h5>${escape(information.content.text)}</h5>
       <hr>
       <div class="article-footer">
-        <h5>${information.created_at}</h5>
+        <h5>${sinceTime(information.created_at)}</h5>
         <small>
           <i class="fas fa-flag"></i>
           <i class="fas fa-retweet"></i>
@@ -28,48 +43,47 @@ const renderTweets = function(tweets) {
       </div>     
     </article>
     `;
-    $("#newsection").prepend($section);
-  }
-}
-
-
-
-const loadTweets = function() { 
-  $.get("/tweets/", function(response) {
-    renderTweets(response);
-  })
-}
-
-const clearArticle = function() {
-  $(".tweets-section").empty();
-}
-
-
-
-const newTweetEvent = function() {
-  $("#form1").submit(function(event) {
-    event.preventDefault();
-    if(!$("#tweet-text").val()) {
-      return alert("You must enter text here !!!")
+      $("#newsection").prepend($section);
     }
-    if($(".counter").val() < 0) {
-      return alert("Maximum character exeeded for the tweet !!!")
+  };
+
+  const warning = function(warn) {
+    $("#warning").slideDown("slow").html(warn);
+  };
+
+  const loadTweets = function() {
+    $.get("/tweets/", function(response) {
+      renderTweets(response);
+    });
+  };
+
+  const clearArticle = function() {
+    $(".tweets-section").empty();
+  };
+
+  const newTweetEvent = function() {
+    $("#form1").submit(function(event) {
+      event.preventDefault();
+      if (!$("#tweet-text").val()) {
+        return warning("You must enter text here !!!");
+      }
+      if ($(".counter").val() < 0) {
+        return warning("Maximum character exeeded for the tweet !!!");
+      }
+
+      $.post("/tweets/", $(this).serialize()).done(function() {
+        clearArticle();
+        loadTweets();
+        $("#tweet-text").val("");
+        warning(" ");
+      }   
+      );
     }
-
-    $.post("/tweets/", $( this ).serialize()).done(function() {
-      clearArticle();
-      loadTweets();
-    $("#tweet-text").val("")
-    }
-      
-    )      
-    }    
-  )}
-
-
-//--------------------------------Function calling ------------------------------------------------
-loadTweets();
-newTweetEvent();
+    );
+  };
+  //--------------------------------Function calling ------------------------------------------------
+  loadTweets();
+  newTweetEvent();
 
 
 });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,16 +1,13 @@
 $(document).ready(function() {
   // --- our code goes here ---
 
-  let count = 140
+  let count = 140;
   $("#tweet-text").keypress( (event) => {
    count --;
    const countetElement = $(event.target).siblings(".counterclass").children("output");
    countetElement.val(count) ;
   if (count <= 0) {
-
-    countetElement.css("color", "red")
+    countetElement.css("color", "red");
   }
   })
- 
- 
 });
