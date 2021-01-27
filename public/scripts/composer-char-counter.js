0f8e876a6e30693a3cb0d2b3d45a4f358be16bcc
Sebastien <perigon@hotmail.com>
2021-01-27 17:13:19 +0000

    Counter bug fix => refactored the way char are counter so that always reflecting the current char count on the screen

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,13 +1,15 @@
 $(document).ready(function() {
-  // --- our code goes here ---
-
   let count = 140;
-  $("#tweet-text").keypress( (event) => {
-   count --;
-   const countetElement = $(event.target).siblings(".counterclass").children("output");
-   countetElement.val(count) ;
-  if (count <= 0) {
+  $("#tweet-text").keydown( (event) => { 
+   const countetElement = $(event.target).siblings(".counterclass").children("output"); 
+   const currentString = $("#tweet-text").val();
+
+  let result = count - currentString.length
+   countetElement.val(result) ;
+  if (result <= 0) {
     countetElement.css("color", "red");
+  } else {
+    countetElement.css("color", "#545149");
   }
   })
 });
